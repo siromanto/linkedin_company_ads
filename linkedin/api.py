6f6cf2780f55586530938df52322b1f9a453cd3e
siromanto <dmytrenko.serhii@gmail.com>
2019-05-15 14:03:24 +0300

    fix api errors

--- linkedin/api.py
+++ linkedin/api.py
@@ -9,7 +9,7 @@ from urllib.parse import urlencode
 
 class LinkedinAdsApi:
 
-    def __init__(self, headers, timeout=30):
+    def __init__(self, headers, timeout=60):
         self.headers = headers
         self.endpoint = 'https://api.linkedin.com/v2/'
         self.h = httplib2.Http("/tmp/.cache", timeout=timeout)
@@ -25,15 +25,15 @@ class LinkedinAdsApi:
 
             if not resp.status == 200:
                 if resp.status == 404:
-                    print('Method does not exists!')
+                    raise Exception('Method does not exists!')
                 elif resp.status == 400:
                     _c = json.loads(content)
                     _ec = _c.get('ErrorCode')
                     if _ec == 8:
-                        print('Invalid parameter given to %s function.' % item)
+                        raise Exception('Invalid parameter given to %s function.' % item)
                     elif _ec == 14:
-                        print('Not authorized.')
-                raise print('Unknown error occured [status code: %d] with response: %s' % (resp.status, content))
+                        raise Exception('Not authorized.')
+                raise Exception('Unknown error occured [status code: %d] with response: %s' % (resp.status, content))
             else:
                 return json.loads(content.decode('utf-8'))
 
